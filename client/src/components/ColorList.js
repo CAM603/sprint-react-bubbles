d03fec2e52a3cb9245ec4125a92df4a0faed8398
CAM603 <cameronhawley@comcast.net>
2020-01-31 12:51:59 -0500

    added add color

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -11,6 +11,10 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [newColor, setNewColor] = useState({
+    color: "",
+    code: { hex: "" }
+  })
   
   const editColor = color => {
     setEditing(true);
@@ -47,6 +51,18 @@ const ColorList = ({ colors, updateColors }) => {
       })
       .catch(err => console.log(err))
   };
+
+  const addColor = (event) => {
+    event.preventDefault()
+    axiosWithAuth()
+      .post(`/colors`, newColor)
+      .then(res => {
+        updateColors(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 
   return (
     <div className="colors-wrap">
@@ -101,8 +117,35 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
-      <div className="spacer" />
-      {/* stretch - build another form here to add a color */}
+      {!editing && (
+        <form onSubmit={addColor}>
+            <legend>Add color</legend>
+            <label>
+              color name:
+              <input
+                onChange={e =>
+                  setNewColor({ ...newColor, color: e.target.value })
+                }
+                value={newColor.color}
+              />
+            </label>
+            <label>
+              hex code:
+              <input
+                onChange={e =>
+                  setNewColor({
+                    ...newColor,
+                    code: { hex: e.target.value }
+                  })
+                }
+                value={newColor.code.hex}
+              />
+            </label>
+            <div className="button-row">
+              <button type="submit">save</button>
+            </div>
+          </form>
+      )}
     </div>
   );
 };
